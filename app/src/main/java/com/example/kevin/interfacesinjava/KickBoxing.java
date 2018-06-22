b8f31be1bc8a3b4e86420a438e6a245ae2df720b
kevin <kevinjobrien420@gmail.com>
2018-06-22 07:29:07 -0500

    236 App Number 29 - Part3 - Interfaces in Java // Coding

--- app/src/main/java/com/example/kevin/interfacesinjava/KickBoxing.java
+++ app/src/main/java/com/example/kevin/interfacesinjava/KickBoxing.java
@@ -1,4 +1,46 @@
 package com.example.kevin.interfacesinjava;
 
-public class KickBoxing {
+public class KickBoxing extends Boxing {
+
+
+    private int kickPowerRequired;
+    private int kickSpeedRequired;
+
+
+    // Constructor for kickboxing class
+
+    public KickBoxing(String name, String rules, String uniform, int speedRequired, int powerRequired,
+                      int punchingPowerRequired, int punchingSpeedRequired,
+                      int kickPowerRequired, int kickSpeedRequired, int costOfInsurance) {
+
+        super(name, rules, uniform, speedRequired, powerRequired, punchingPowerRequired,
+                punchingSpeedRequired, costOfInsurance);
+        this.kickPowerRequired = kickPowerRequired;
+        this.kickSpeedRequired = kickSpeedRequired;
+    }
+
+
+    public int getKickPowerRequired() {
+        return kickPowerRequired;
+    }
+
+    public void setKickPowerRequired(int kickPowerRequired) {
+        this.kickPowerRequired = kickPowerRequired;
+    }
+
+    public int getKickSpeedRequired() {
+        return kickSpeedRequired;
+    }
+
+    public void setKickSpeedRequired(int kickSpeedRequired) {
+        this.kickSpeedRequired = kickSpeedRequired;
+    }
+
+    @Override
+    public String toString() {
+
+        return String.format("%s%n%s: %d%n%s: %d", super.toString(),
+                "Kick Power Required", getKickPowerRequired(),
+                "Kick Speed Required", getKickPowerRequired());
+    }
 }
--- app/src/main/java/com/example/kevin/interfacesinjava/Student.java
+++ app/src/main/java/com/example/kevin/interfacesinjava/Student.java
@@ -1,4 +1,49 @@
 package com.example.kevin.interfacesinjava;
 
-public class Student {
+public class Student implements LifeInsurance{
+
+    private String name;
+    private String courseName;
+    private int costOfInsurance;
+
+
+    // Student Constructor
+    public Student(String name, String courseName, int costOfInsurance) {
+
+        this.name = name;
+        this.courseName = courseName;
+        this.costOfInsurance = costOfInsurance;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getCostOfInsurance() {
+        return costOfInsurance;
+    }
+
+    public void setCostOfInsurance(int costOfInsurance) {
+        this.costOfInsurance = costOfInsurance;
+    }
+
+    @Override
+    public double evaluateCostOfInsurance() {
+
+        return  getCostOfInsurance();
+
+    }
 }
